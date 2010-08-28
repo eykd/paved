fe5a71264bed341d29c1c623d377cf7d7cd382b1
David Eyk <eykd@eykd.net>
2010-08-28 17:35:12 -0500

    Added more django commands.

--- paved/django.py
+++ paved/django.py
@@ -4,12 +4,16 @@
 Copyright 2010 David Eyk. All rights reserved.
 """
 from paver.easy import options, sh, task, consume_args, Bunch
+from paver.runtime import BuildFailure
 
 from . import paved
 
 options.paved.update(
     django = Bunch(
         manage_py = None,
+        syncdb = Bunch(
+            fixtures = [],
+            ),
         ),
     )
 
@@ -31,3 +35,44 @@ def call_manage(cmd, error):
         error("No manage.py defined. Use: options.paved.django(manage_py = 'path_to_manage_py')")
         raise ValueError()
     sh('python {manage_py} {cmd}'.format(**locals()))
+
+
+@task
+@consume_args
+def test(args):
+    """Run tests.
+    """
+    cmd = args and 'test %s' % ' '.join(options.args) or 'test'
+    call_manage(cmd)
+
+
+@task
+@consume_args
+def syncdb(args):
+    """Update the database with model schema.
+    """
+    cmd = args and 'syncdb %s' % ' '.join(options.args) or 'syncdb --noinput'
+    call_manage(cmd)
+    for fixture in options.paved.django.syncdb.fixtures:
+        call_manage("loaddata %s" % fixture)
+
+
+@task
+def shell():
+    """Run the ipython shell.
+    """
+    try:
+        call_manage('shell_plus')
+    except BuildFailure:
+        call_manage('shell')
+
+
+@task
+def start():
+    """Run the dev server.
+    """
+    try:
+        call_manage('runserver_plus')
+    except BuildFailure:
+        call_manage('runserver')
+
