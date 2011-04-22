4fecff25222ff2c109a51e3dfc7ed89008b77ad2
ponty <titi@pince.(none)>
2011-04-22 21:24:18 +0200

    add sphinx path to manifest

--- paved/dist.py
+++ paved/dist.py
@@ -25,9 +25,12 @@ util.update(
                         "CHANGES*",
                         "LICENSE*",
                         "README*",
+                        "TODO*",
                         ]),
                 recursive_include = set([]),
                 prune = set([]),
+                include_sphinx_docroot=True,
+                exclude_sphinx_builddir=True,                
                 ),
             )
         )
@@ -52,18 +55,38 @@ def get_distribute(options):
 def manifest():
     """Guarantee the existence of a basic MANIFEST.in.
 
+    manifest doc: http://docs.python.org/distutils/sourcedist.html#manifest
+    
     `options.paved.dist.manifest.include`: set of files (or globs) to include with the `include` directive.
 
     `options.paved.dist.manifest.recursive_include`: set of files (or globs) to include with the `recursive-include` directive.
 
     `options.paved.dist.manifest.prune`: set of files (or globs) to exclude with the `prune` directive.
+    
+    `options.paved.dist.manifest.include_sphinx_docroot`: True -> sphinx docroot is added as `grant`
+    
+    `options.paved.dist.manifest.include_sphinx_docroot`: True -> sphinx builddir is added as `prune`
     """
+    prune = options.paved.dist.manifest.prune
+    grant = set()
+    
+    
+    if options.paved.dist.manifest.include_sphinx_docroot:
+        docroot = options.get('docroot', 'docs')
+        grant.update([docroot])
+    
+        if options.paved.dist.manifest.exclude_sphinx_builddir:
+            builddir = docroot + '/' + options.get("builddir", ".build")
+            prune.update([builddir])
+         
     with open(options.paved.cwd / 'MANIFEST.in', 'w') as fo:
+        for item in grant:
+            fo.write('grant %s\n' % item)
         for item in options.paved.dist.manifest.include:
             fo.write('include %s\n' % item)
         for item in options.paved.dist.manifest.recursive_include:
             fo.write('recursive-include %s\n' % item)
-        for item in options.paved.dist.manifest.prune:
+        for item in prune:
             fo.write('prune %s\n' % item)
 
 
