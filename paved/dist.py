0331d6a7b9d30a5e3bb2f29415235e6f87a44c4b
ponty <ponty@home>
2013-01-21 22:03:19 +0100

    Fix typo

--- paved/dist.py
+++ paved/dist.py
@@ -63,25 +63,25 @@ def manifest():
 
     `options.paved.dist.manifest.prune`: set of files (or globs) to exclude with the `prune` directive.
     
-    `options.paved.dist.manifest.include_sphinx_docroot`: True -> sphinx docroot is added as `grant`
+    `options.paved.dist.manifest.include_sphinx_docroot`: True -> sphinx docroot is added as `graft`
     
     `options.paved.dist.manifest.include_sphinx_docroot`: True -> sphinx builddir is added as `prune`
     """
     prune = options.paved.dist.manifest.prune
-    grant = set()
+    graft = set()
     
     
     if options.paved.dist.manifest.include_sphinx_docroot:
         docroot = options.get('docroot', 'docs')
-        grant.update([docroot])
+        graft.update([docroot])
     
         if options.paved.dist.manifest.exclude_sphinx_builddir:
             builddir = docroot + '/' + options.get("builddir", ".build")
             prune.update([builddir])
          
     with open(options.paved.cwd / 'MANIFEST.in', 'w') as fo:
-        for item in grant:
-            fo.write('grant %s\n' % item)
+        for item in graft:
+            fo.write('graft %s\n' % item)
         for item in options.paved.dist.manifest.include:
             fo.write('include %s\n' % item)
         for item in options.paved.dist.manifest.recursive_include:
