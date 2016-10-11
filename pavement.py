38b41176afefbad01e72dd41a2f2b8ac1e00a755
Ryan Rubin <ryanmrubin@gmail.com>
2016-10-11 13:51:28 -0500

    bump version to 0.5.0

--- pavement.py
+++ pavement.py
@@ -26,7 +26,7 @@ options.paved.docs.rsync_location = 'eykd.net:webapps/net_eykd_worlds__static/pa
 
 setup(
     name = "Paved",
-    version = "0.4.1",
+    version = "0.5.0",
     url = "https://github.com/eykd/paved",
     download_url = "http://pypi.python.org/pypi/Paved/",
     author = "David Eyk",
@@ -46,6 +46,8 @@ setup(
         'Topic :: Utilities',
         'Topic :: Software Development :: Build Tools',
         'Topic :: Software Development :: Libraries :: Python Modules',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
         ],
 
     packages=['paved'],
