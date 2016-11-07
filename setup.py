6a6fe9bb850d2dfe42f210e4ba752373e0c5a5df
Ryan Rubin <ryanmrubin@gmail.com>
2016-11-07 13:42:21 -0600

    use vanilla setuptools setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,36 @@
-from __future__ import unicode_literals
+import sys
 import os
-if os.path.exists("paver-minilib.zip"):
-    import sys
-    sys.path.insert(0, "paver-minilib.zip")
 
-import paver.tasks
-paver.tasks.main()
+from setuptools import setup
+
+setup(
+    name = "Paved",
+    version = "0.5.0",
+    url = "https://github.com/eykd/paved",
+    download_url = "http://pypi.python.org/pypi/Paved/",
+    author = "David Eyk",
+    author_email = "eykd@eykd.net",
+    license = 'BSD',
+
+    description = 'Common tasks for Paver-based projects.',
+    long_description = open('README.rst').read(),
+
+    classifiers = [
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Natural Language :: English',
+        'Topic :: System :: Software Distribution',
+        'Topic :: Utilities',
+        'Topic :: Software Development :: Build Tools',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+        ],
+
+    packages=['paved'],
+    install_requires = ['Paver'],
+    include_package_data = True,
+    zip_safe = False,
+)
